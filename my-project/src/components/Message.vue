512c693559504c906cc091f0f9a6e7dbb69789fd
Nattamai Gopinath <r.nattamai.gopinath@accenture.com>
2022-02-09 10:12:32 +0530

    First commit

---
+++ my-project/src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<input :value="msg" @input="changeMessage">
+<p>{{msg}}</p>
+</div>
+</template>
+<script>
+export default {
+  props: ['msg'],
+  methods: {
+    changeMessage (event) {
+      this.$emit('messageChanged', event.target.value)
+    }
+  }
+}
+</script>
